aac815832ee386d26e0261f8a32589403c53393e
Kirill Filippov <vip.kirya.filippov.2002@gmail.com>
2025-05-29 13:27:29 +0400

    Фикс карточки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Card } from '@/ui/Card'
+
 export default function Home() {
   return (
     <div>
@@ -5,6 +7,11 @@ export default function Home() {
       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
       <a>Products</a>
+      <Card
+        title="title"
+        text="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+        picture="/img/cards/1.png"
+      />
     </div>
   )
 }
