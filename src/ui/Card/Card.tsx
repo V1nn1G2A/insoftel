074857bf21293d108d22028388b2507e2525aa0c
Danil Ochirov <d.ochirov@framework.team>
2025-05-29 15:52:58 +0400

    fix: fix Card for new mixins and use new Image component

--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -1,5 +1,9 @@
+'use client'
+
+import cn from 'classnames'
 import Image from 'next/image'
 import type { FC } from 'react'
+import { useState } from 'react'
 
 import PlusIcon from '@assets/icons/plus.svg'
 
@@ -12,26 +16,43 @@ interface ICard {
   picture: string
 }
 
-const Card: FC<ICard> = ({ picture, title, text, className }) => (
-  <div className={`${styles.card} ${className}`}>
-    <Image
-      src={picture}
-      width={435}
-      height={434}
-      alt={title}
-    />
-    <div className={styles.card__content}>
-      <div className={styles.card__title}>
-        <p>{title}</p>
-        <button>
-          <PlusIcon />
-        </button>
-      </div>
-      <div className={styles.card__text}>
-        <p>{text}</p>
+const cx = cn.bind(styles)
+
+const Card: FC<ICard> = ({ picture, title, text, className }) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const classNames = cx(styles.card, className, {
+    [styles['card--active']]: isActive,
+  })
+
+  const handleOpen = () => {
+    setIsActive(prev => !prev)
+  }
+
+  return (
+    <div
+      className={classNames}
+      onClick={handleOpen}
+    >
+      <Image
+        src={picture}
+        width={435}
+        height={434}
+        alt={title}
+      />
+      <div className={styles.card__content}>
+        <div className={styles.card__title}>
+          <p>{title}</p>
+          <button>
+            <PlusIcon />
+          </button>
+        </div>
+        <div className={styles.card__text}>
+          <p>{text}</p>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Card
