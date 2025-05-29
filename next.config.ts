711cbbbdca33294859bddb294a7241abebff6c89
Nikita Mironov <nmironov2002@gmail.com>
2025-05-29 12:41:24 +0400

    webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import type { RuleSetRule } from 'webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(config) {
+    const rules = config.module.rules as RuleSetRule[]
 
-export default nextConfig;
+    const fileLoaderRule = rules.find(
+      rule =>
+        typeof rule === 'object' &&
+        rule !== null &&
+        rule.test instanceof RegExp &&
+        rule.test.test('.svg')
+    )
+
+    if (!fileLoaderRule) {
+      throw new Error('SVG file loader rule not found')
+    }
+
+    rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [/url/] },
+        use: ['@svgr/webpack'],
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
+}
+
+export default nextConfig
