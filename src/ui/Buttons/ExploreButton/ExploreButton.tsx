3782fc56373354350911f214ad50874f2604d717
Kirill Filippov <vip.kirya.filippov.2002@gmail.com>
2025-05-30 16:04:32 +0400

    fix: classname

--- src/ui/Buttons/ExploreButton/ExploreButton.tsx
+++ src/ui/Buttons/ExploreButton/ExploreButton.tsx
@@ -1,6 +1,6 @@
-import ArrowIcon from '@/assets/icons/arrow.svg'
-
 import classNames from 'classnames/bind'
+
+import ArrowIcon from '@/assets/icons/arrow.svg'
 
 import styles from './exploreButton.module.scss'
 
@@ -9,15 +9,15 @@ const cx = classNames.bind(styles)
 interface IExploreButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string
   colorVariant?: 'light' | 'dark'
-  buttonClassName?: string
+  className?: string
 }
 
 const ExploreButton: React.FC<IExploreButton> = ({
   text = 'Explore',
   colorVariant = 'light',
-  buttonClassName,
+  className,
 }) => (
-  <button className={cx('button', colorVariant, buttonClassName)}>
+  <button className={cx('button', colorVariant, className)}>
     <p className={cx('text', colorVariant)}>{text}</p>
     <div className={cx('round', colorVariant)}>
       <ArrowIcon className={cx('arrow', colorVariant)} />
