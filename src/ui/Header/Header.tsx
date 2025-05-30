407841cc8647ba4ea28db0f67dbc826c7950ec54
Danil Ochirov <d.ochirov@framework.team>
2025-05-30 14:42:19 +0400

    feat: update props for Header component

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -5,20 +5,29 @@ import styles from './Header.module.scss'
 
 interface IHeader {
   className?: string
-  text: string
+  children: string
   index: string
-  theme?: 'light' | 'dark'
+  appereance?: 'light' | 'dark'
 }
 
 const cx = cn.bind(styles)
 
-const Header: FC<IHeader> = ({ className, text, index, theme = 'light' }) => {
-  const classNames = cx(styles.header, styles[`header--${theme}`], className)
+const Header: FC<IHeader> = ({
+  className,
+  children,
+  index,
+  appereance = 'light',
+}) => {
+  const classNames = cx(
+    styles.header,
+    styles[`header--${appereance}`],
+    className
+  )
 
   return (
     <div className={classNames}>
       <sup className={styles.index}>[ {index} ]</sup>
-      <h2 className={styles.text}>{text}</h2>
+      <h2 className={styles.text}>{children}</h2>
     </div>
   )
 }
