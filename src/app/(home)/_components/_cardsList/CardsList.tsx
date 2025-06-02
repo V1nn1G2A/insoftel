61957438447943de2411ae8869ffce440ccadca1
Danil Ochirov <d.ochirov@framework.team>
2025-06-02 13:13:34 +0400

    feat: create CardsList component

---
+++ src/app/(home)/_components/_cardsList/CardsList.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import type { FC } from 'react'
+import { useRef } from 'react'
+import type SwiperCore from 'swiper'
+import { Keyboard } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { PaginatedButton } from '@/ui/Buttons/PaginatedButton'
+import { TextButton } from '@/ui/Buttons/TextButton'
+import { Card } from '@/ui/Card'
+
+import cards from '../../_constants/CARD'
+
+import styles from './CardsList.module.scss'
+
+const CardsList: FC = ({}) => {
+  const swiperRef = useRef<SwiperCore | null>(null)
+
+  return (
+    <div className={styles.cards}>
+      <Swiper
+        onSwiper={swiper => (swiperRef.current = swiper)}
+        keyboard={{ enabled: true }}
+        modules={[Keyboard]}
+        slidesPerView={'auto'}
+      >
+        {cards.map(card => (
+          <SwiperSlide
+            key={card.title}
+            className={styles.slide}
+          >
+            <Card
+              title={card.title}
+              text={card.content}
+              picture={card.picture}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={styles.cards__btns}>
+        <div className={styles.cards__pagination}>
+          <PaginatedButton
+            orientation="left"
+            onClick={() => swiperRef.current?.slidePrev()}
+          />
+          <PaginatedButton
+            orientation="right"
+            onClick={() => swiperRef.current?.slideNext()}
+          />
+        </div>
+        <TextButton
+          text="Explore Our Products"
+          variant="short"
+          colorVariant="dark"
+          className={styles.cards__explore}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default CardsList
