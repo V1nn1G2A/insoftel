e0cbdb798602408daf9aa6482499ef639d21a7a1
Danil Ochirov <d.ochirov@framework.team>
2025-06-02 13:19:35 +0400

    feat: update Accordion component

--- src/ui/Accordion/Accordion.tsx
+++ src/ui/Accordion/Accordion.tsx
@@ -1,5 +1,7 @@
-import Arrow from '@assets/icons/arrow.svg'
+import cn from 'classnames'
 import type { FC } from 'react'
+
+import Arrow from '@/assets/icons/arrow.svg'
 
 import styles from './Accordion.module.scss'
 
@@ -8,39 +10,50 @@ interface IAccordion {
   className?: string
   content: string[]
   isLine?: boolean
+  isActive?: boolean
+  onClick: () => void
 }
+
+const cx = cn.bind(styles)
 
 const Accordion: FC<IAccordion> = ({
   title,
   className,
   content,
   isLine = true,
-}) => (
-  <div className={`${styles.accordion} ${className}`}>
-    <input
-      type="checkbox"
-      id={`${title}-toggle`}
-      className={styles.toggle}
-    />
-    <label
-      htmlFor={`${title}-toggle`}
-      className={styles.accordion__title}
-    >
-      <p>
-        [ {title} ]
-        <sup className={styles['card__content-counter']}>{content.length}</sup>
-      </p>
-      <span className={styles.accordion__btn}>
-        <Arrow />
-      </span>
-    </label>
-    <div className={styles.accordion__content}>
-      <div>
-        <p>{content.join(', ')}</p>
+  isActive,
+  onClick,
+}) => {
+  const classNames = cx(
+    styles.accordion,
+    { [styles['accordion--active']]: isActive },
+    className
+  )
+
+  return (
+    <div className={classNames}>
+      <div
+        className={styles.accordion__title}
+        onClick={onClick}
+      >
+        <p>
+          [ {title} ]
+          <sup className={styles['card__content-counter']}>
+            {content.length}
+          </sup>
+        </p>
+        <span className={styles.accordion__btn}>
+          <Arrow />
+        </span>
       </div>
+      <div className={styles.accordion__content}>
+        <div>
+          <p>{content.join(', ')}</p>
+        </div>
+      </div>
+      {isLine && <span className={styles.accordion__line}></span>}
     </div>
-    {isLine && <span className={styles.accordion__line}></span>}
-  </div>
-)
+  )
+}
 
 export default Accordion
