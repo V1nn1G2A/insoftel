c4510ca11f4d046662e9a4234618897011499d71
Nikita Mironov <nmironov2002@gmail.com>
2025-06-02 17:57:01 +0400

    contacts + начал фиксить services и добавлять логику пагинации

---
+++ src/app/contacts/_constants/CONTACTS.ts
@@ -0,0 +1,15 @@
+export const addresses = [
+  {
+    title: '[ Head Office – Dubai ]',
+    text: 'The Binary Tower, 32 Marasi Drive St., Business Bay, Dubai, UAE',
+  },
+  {
+    title: '[ Office – Istanbul ]',
+    text:
+      'Business Center, Nur Sokak 2A, Merdivenköy, Kadıköy, Istanbul, Turkey',
+  },
+  {
+    title: '[ Email ]',
+    text: 'info@insofteltechnologies.com',
+  },
+]
