8976f5ba5d3ea0d776426428330de7ecaac3a337
Danil Ochirov <d.ochirov@framework.team>
2025-06-05 18:49:26 +0400

    feat: animate Contacts page

--- src/app/contacts/_components/Heading/index.tsx
+++ src/app/contacts/_components/Heading/index.tsx
@@ -1,20 +1,56 @@
+'use client'
+
+import { useRef } from 'react'
+
+import { AnimatedLine, AnimatedText, AnimationBlock, BigLetter } from '@/ui'
+
 import styles from './index.module.scss'
 
-const Heading = () => (
-  <div className={styles.heading}>
-    <div className={styles.content}>
-      <h1 className={styles.title}>We’d Love to Hear from You!</h1>
-      <p className={styles.text}>
-        Whether you have a question about our services, are interested in a
-        partnership, or want to learn more about our company, feel free to reach
-        out.
-      </p>
+const Heading = () => {
+  const ref = useRef(null)
+
+  return (
+    <div
+      className={styles.heading__wrapper}
+      ref={ref}
+    >
+      <div className={styles.heading}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            <AnimatedText
+              height={100}
+              className={styles.line}
+            >
+              We’d Love to
+            </AnimatedText>
+            <AnimatedText
+              height={100}
+              className={styles.line}
+            >
+              Hear from You!
+            </AnimatedText>
+          </h1>
+          <AnimationBlock
+            type="p"
+            className={styles.text}
+          >
+            Whether you have a question about our services, are interested in a
+            partnership, or want to learn more about our company, feel free to
+            reach out.
+          </AnimationBlock>
+        </div>
+        <div className={styles.letters}>
+          <BigLetter className={styles.s}>S</BigLetter>
+          <BigLetter className={styles.t}>T</BigLetter>
+        </div>
+      </div>
+      <AnimatedLine
+        targetRef={ref}
+        className={styles.border}
+        x={-1400}
+      />
     </div>
-    <div className={styles.letters}>
-      <span className={styles.s}>S</span>
-      <span className={styles.t}>T</span>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Heading
