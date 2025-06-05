cdb0b3805ff9ea614663001af9ca33c58fe6e3af
Nikita Mironov <nmironov2002@gmail.com>
2025-06-05 10:31:58 +0400

    Поправил отображение SectionHeading

--- src/ui/SectionHeading/index.tsx
+++ src/ui/SectionHeading/index.tsx
@@ -10,27 +10,47 @@ const SectionHeading = ({
 }: {
   title: string
   letter: string
-}) => (
-  <VideoBackground
-    src={'/video/section-background.mp4'}
-    className="section"
-  >
-    <Container>
-      <div className={styles.heading}>
-        <span className={styles.letter}>{letter}</span>
-        <h1 className={styles.title}>{title}</h1>
-        <h1 className={styles.miniTitle}>{title.toUpperCase()}</h1>
-        <div className={styles.miniButton}>
-          <ExploreButton
-            classNames={['mini-text', 'mini-round', 'mini-button']}
-          />
-        </div>
-        <div className={styles.button}>
-          <ExploreButton classNames={['mini-text']} />
-        </div>
-      </div>
-    </Container>
-  </VideoBackground>
-)
+}) => {
+  let width
 
+  switch (title) {
+    case 'Services':
+      width = '72.79px'
+      break
+    case 'Technologies':
+      width = '117.68px'
+  }
+
+  return (
+    <VideoBackground
+      src={'/video/section-background.mp4'}
+      className="section"
+    >
+      <Container>
+        <div className={styles.heading}>
+          <span className={styles.letter}>{letter}</span>
+          <h1 className={styles.title}>{title}</h1>
+          <h1
+            className={styles.miniTitle}
+            style={
+              { '--width': width } as React.CSSProperties & {
+                [key: string]: string
+              }
+            }
+          >
+            {title.toUpperCase()}
+          </h1>
+          <div className={styles.miniButton}>
+            <ExploreButton
+              classNames={['mini-text', 'mini-round', 'mini-button']}
+            />
+          </div>
+          <div className={styles.button}>
+            <ExploreButton classNames={['mini-text']} />
+          </div>
+        </div>
+      </Container>
+    </VideoBackground>
+  )
+}
 export default SectionHeading
