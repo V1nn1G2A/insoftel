54f6cefe0e5f3c8a08a97e513fbe572ea5364774
smerdit9 <nmironov2002@gmail.com>
2025-06-10 09:19:43 +0400

    fix not-found: add video

--- src/ui/NotFound/index.tsx
+++ src/ui/NotFound/index.tsx
@@ -1,10 +1,13 @@
+import VideoBackground from '../VideoBackground'
 import NotFoundContent from './NotFoundContent'
 import styles from './index.module.scss'
 
 const NotFoundPage = () => (
-    <div className={styles.error__page}>
-        <NotFoundContent code='404' message='Page Not Found' />
-    </div>
+    <VideoBackground src={'/video/not-found.mp4'} className={styles.video}>
+        <div className={styles.error__page}>
+            <NotFoundContent code='404' message='Page Not Found' />
+        </div>
+    </VideoBackground>
 )
 
 export default NotFoundPage
