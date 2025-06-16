a32221039ad210eb2be72c693169f86b7795d008
Danil Ochirov <d.ochirov@framework.team>
2025-06-16 10:16:01 +0400

    feat: update careers Form

---
+++ src/app/careers/constants/OPENING.ts
@@ -0,0 +1,7 @@
+const OPENING = [
+  { title: 'Sr. Product Designer', id: 'Product' },
+  { title: 'Sr. UX Designer', id: 'UX' },
+  { title: 'Sr. Front-end Developer Freelance', id: 'Front' },
+]
+
+export default OPENING
