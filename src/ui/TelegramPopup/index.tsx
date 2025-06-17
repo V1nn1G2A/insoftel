076a4937ea23177cb9ca14787fe2b32d42df0a6b
Danil Ochirov <d.ochirov@framework.team>
2025-06-17 18:20:01 +0400

    fix: popups

--- src/ui/TelegramPopup/index.tsx
+++ src/ui/TelegramPopup/index.tsx
@@ -2,20 +2,32 @@
 
 import { TextButton } from '../Buttons'
 import { usePopupDispatch } from '../Popup/PopupContext'
+
 import styles from './index.module.scss'
 
 const TelegramPopup = () => {
-    const { closePopup } = usePopupDispatch();
-    
-   return (
-        <div className={styles.popup__content}>
-            <p className={styles.popup__text}>By clicking &quot;Confirm,&quot; you will be redirected to an Telegram chat</p>
-            <div className={styles.popup__buttons}>
-            <button className={styles.popup__cancel} onClick={closePopup}>Cancel</button>
-            <TextButton text='Confirm' variant='short' colorVariant='dark' />
-            </div>
-        </div>
-    )
+  const { closePopup } = usePopupDispatch()
 
+  return (
+    <div className={styles.popup__content}>
+      <p className={styles.popup__text}>
+        By clicking &quot;Confirm&quot;, you will be redirected to an Telegram
+        chat
+      </p>
+      <div className={styles.popup__buttons}>
+        <button
+          className={styles.popup__cancel}
+          onClick={closePopup}
+        >
+          Cancel
+        </button>
+        <TextButton
+          text="Confirm"
+          variant="short"
+          colorVariant="dark"
+        />
+      </div>
+    </div>
+  )
 }
 export default TelegramPopup
